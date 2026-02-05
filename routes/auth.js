ed1ca77aab5460f4ffbbf5d4931cd1fe34188993
gaurav.tomer <nikhil.dhomne>
2026-02-06 00:33:47 +0530

    added login api

--- routes/auth.js
+++ routes/auth.js
@@ -48,7 +48,6 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     console.error("Registration error:", error);
 
-
     if (error.code === 11000) {
       return res.status(400).json({
         success: false,
@@ -63,5 +62,63 @@ router.post("/register", async (req, res) => {
     });
   }
 });
+
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    
+    const user = await UserSetup.findOne({ email }).select("+password");
+
+    
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Wrong email",
+        field: "email",
+      });
+    }
+
+    
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+   
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Wrong password",
+        field: "password",
+      });
+    }
+
+    
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        userId: user.userId,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+});
+
+
 
 export default router;
