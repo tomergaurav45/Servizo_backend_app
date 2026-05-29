927bcc8362f438fd4260e1cdfa1d33772b500cf8
gaurav.tomer <nikhil.dhomne>
2026-05-30 00:29:57 +0530

    change in notification

--- models/notification.js
+++ models/notification.js
@@ -18,4 +18,8 @@ const notificationSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-export default mongoose.model("Notification", notificationSchema);
+const Notification =
+  mongoose.models.Notification ||
+  mongoose.model("Notification", notificationSchema);
+
+export default Notification;
